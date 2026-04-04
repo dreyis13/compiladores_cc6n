7f1e75af1069aa5865077ec0ed6234be5c40a186
dreyis13 <10420696@mackenzista.com.br>
2026-04-04 20:37:37 -0300

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,13 @@
+/*
+Andrey Bezerra Virginio dos Santos - 10420696
+Igor Silva Araujo - 10428505
+*/
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+
+void parse_program(FILE *source, const char *source_filename, int trace_flag, int tokens_flag, int symtab_flag);
+
+#endif
