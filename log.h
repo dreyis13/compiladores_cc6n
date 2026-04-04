1954cc5320f1a7d36e7d62fac46134f166bb265d
dreyis13 <10420696@mackenzista.com.br>
2026-04-04 20:59:35 -0300

    Update log.h

--- log.h
+++ log.h
@@ -8,10 +8,12 @@ Igor Silva Araujo - 10428505
 
 #include "lex.h"
 
+/* Funções para gerenciar o arquivo de lista de tokens (.tk) */
 void log_open_tokens(const char *base_filename);
 void log_write_token(Token tok);
 void log_close_tokens(void);
 
+/* Funções para gerenciar o arquivo de rastreamento (.trc) */
 void log_open_trace(const char *base_filename);
 void log_write_trace(const char *msg);
 void log_close_trace(void);
