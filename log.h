dfac77f62353de0f0d7b2f0a6cda4229e471d4c5
dreyis13 <10420696@mackenzista.com.br>
2026-04-04 20:36:08 -0300

    Create log.h

---
+++ log.h
@@ -0,0 +1,19 @@
+/*
+Andrey Bezerra Virginio dos Santos - 10420696
+Igor Silva Araujo - 10428505
+*/
+
+#ifndef LOG_H
+#define LOG_H
+
+#include "lex.h"
+
+void log_open_tokens(const char *base_filename);
+void log_write_token(Token tok);
+void log_close_tokens(void);
+
+void log_open_trace(const char *base_filename);
+void log_write_trace(const char *msg);
+void log_close_trace(void);
+
+#endif
