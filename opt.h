8ada289794114d7805be954a340dd4136e28438a
dreyis13 <10420696@mackenzista.com.br>
2026-04-04 21:03:19 -0300

    Update opt.h

--- opt.h
+++ opt.h
@@ -6,14 +6,18 @@ Igor Silva Araujo - 10428505
 #ifndef OPT_H
 #define OPT_H
 
+/* Estrutura que armazena as opções passadas pela linha de comando */
 typedef struct {
-    char *source_file;
-    int tokens_flag;
-    int symtab_flag;
-    int trace_flag;
+    char *source_file;    // nome do arquivo fonte .sal
+    int tokens_flag;      // ativa geração do arquivo .tk (lista de tokens)
+    int symtab_flag;      // ativa geração do arquivo .ts (tabela de símbolos)
+    int trace_flag;       // ativa geração do arquivo .trc (rastreamento)
 } Options;
 
+/* Processa os argumentos da linha de comando e preenche a estrutura */
 void opt_parse(int argc, char *argv[], Options *opts);
+
+/* Libera a memória alocada para o nome do arquivo fonte */
 void opt_cleanup(Options *opts);
 
 #endif
