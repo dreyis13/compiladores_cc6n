1cec6a02cbec7bf38372108d530d3a1d7fb96adb
dreyis13 <10420696@mackenzista.com.br>
2026-04-04 21:09:14 -0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -8,6 +8,8 @@ Igor Silva Araujo - 10428505
 
 #include <stdio.h>
 
+/* Função principal da análise sintática: recebe o arquivo fonte, o nome base para logs,
+   e flags para ativar trace, tokens e tabela de símbolos. */
 void parse_program(FILE *source, const char *source_filename, int trace_flag, int tokens_flag, int symtab_flag);
 
 #endif
